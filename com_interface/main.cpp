80ed7d734edd89b2d811b90bf17b8f9502472e9b
james.huang <james.huang1023@gmail.com>
2015-05-19 09:58:55 +0800

    Initial Commit

---
+++ com_interface/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  com_interface
+//
+//  Created by james.huang on 5/19/15.
+//  Copyright (c) 2015 james.huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
